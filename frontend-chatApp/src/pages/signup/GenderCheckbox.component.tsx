d389080f61b0c736c19d6466e06dc5e9dc35d7df
Sebastian Tixilema <sebastixi2002@gmail.com>
2024-11-28 21:37:13 -0500

    Arreglado los estilos de los checkbox y otros

--- frontend-chatApp/src/pages/signup/GenderCheckbox.component.tsx
+++ frontend-chatApp/src/pages/signup/GenderCheckbox.component.tsx
@@ -1,34 +1,39 @@
 import { UserSignUp } from "../../types";
 
 type GenderCheckboxProps = {
-    handleChange: (e: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLSelectElement>) => void
+    handleChange: (e: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLSelectElement>) => void;
     input: UserSignUp['gender'];
-}
-
+};
 
 export default function GenderCheckbox({ handleChange, input }: GenderCheckboxProps) {
     return (
-        <div className='flex'>
+        <div className='flex items-center gap-6'> {/* Espaciado horizontal entre los checkboxes */}
             <div className='form-control'>
-                <label className='label gap-2 cursor-pointer'>
-                    <span className='label-text'>Male</span>
-                    <input type="checkbox" className='checkbox border-slate-900'
+                <label className='label cursor-pointer flex items-center gap-2'>
+                    <input 
+                        type="checkbox" 
+                        className='checkbox border-2 border-white focus:ring-2 focus:ring-blue-500'
                         name="gender"
                         checked={input === "male"}
-                        value={"male"}
-                        onChange={handleChange} />
+                        value="male"
+                        onChange={handleChange} 
+                    />
+                    <span className='label-text text-sm font-medium text-white'>Male</span>
                 </label>
             </div>
             <div className='form-control'>
-                <label className='label gap-2 cursor-pointer'>
-                    <span className='label-text'>Female</span>
-                    <input type="checkbox" className='checkbox border-slate-900'
+                <label className='label cursor-pointer flex items-center gap-2'>
+                    <input 
+                        type="checkbox" 
+                        className='checkbox border-2 border-white focus:ring-2 focus:ring-blue-500'
                         name="gender"
                         checked={input === "female"}
-                        value={"female"}
-                        onChange={handleChange} />
+                        value="female"
+                        onChange={handleChange} 
+                    />
+                    <span className='label-text text-sm font-medium text-white'>Female</span>
                 </label>
             </div>
         </div>
-    )
+    );
 }
