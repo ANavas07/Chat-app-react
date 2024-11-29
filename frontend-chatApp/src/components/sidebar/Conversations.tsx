02e0f1de7d8c977132a6e6a254f0383e7f34e779
Sebastian Tixilema <sebastixi2002@gmail.com>
2024-11-29 18:27:13 -0500

    Buscador de usuarios

--- frontend-chatApp/src/components/sidebar/Conversations.tsx
+++ frontend-chatApp/src/components/sidebar/Conversations.tsx
@@ -1,21 +1,33 @@
+import { useState, useEffect } from "react";
 import UseGetConversations from "../../hooks/UserConversations";
 import { UserConversation } from "../../types";
 import Conversation from "./Conversation";
 
-export default function Conversations() {
-    const {loading, conversations}= UseGetConversations();
+export default function Conversations({ search }: { search: string }) {
+    const { loading, conversations } = UseGetConversations();
+    const [filteredConversations, setFilteredConversations] = useState<UserConversation[]>([]);
+
+    // Filtrar las conversaciones según el texto de búsqueda
+    useEffect(() => {
+        if (search) {
+            setFilteredConversations(
+                conversations.filter(c => c.fullName.toLowerCase().includes(search.toLowerCase()))
+            );
+        } else {
+            setFilteredConversations(conversations);
+        }
+    }, [search, conversations]);
+
     return (
-        <div className="py-2 flex flex-col overflow-auto h-[87%]">
-            {conversations.map((conversation:UserConversation, index:number)=>{
-                return (
-                    <Conversation 
-                        key={conversation._id}
-                        conversation={conversation}
-                        lastIndex={index == conversations.length - 1}
-                    />
-                );
-            })}
-            {loading ? <span className="loading loading-spinner"></span>: null}
+        <div className="py-2 flex flex-col overflow-auto h-[80%]">
+            {filteredConversations.map((conversation: UserConversation, index: number) => (
+                <Conversation
+                    key={conversation._id}
+                    conversation={conversation}
+                    lastIndex={index === filteredConversations.length - 1}
+                />
+            ))}
+            {loading ? <span className="loading loading-spinner"></span> : null}
         </div>
-    )
+    );
 }
--- frontend-chatApp/src/components/sidebar/Sidebar.tsx
+++ frontend-chatApp/src/components/sidebar/Sidebar.tsx
@@ -1,15 +1,17 @@
+import { useState } from "react";
 import useConversations from "../../zustand/ZConversations";
+import SearchInput from "./SearchInput";
 import Conversations from "./Conversations";
 import LogoutButton from "./LogoutButton";
-import SearchInput from "./SearchInput";
 
 export default function Sidebar() {
     const { selectedConversation } = useConversations();
+    const [search, setSearch] = useState('');
     return (
         <div className={`${!selectedConversation ? 'block' : 'hidden'} sm:block border-r border-slate-500 p-4 flex flex-col sm:w-full sm:h-full `}>
-            <SearchInput />
+            <SearchInput search={search} setSearch={setSearch} />
             <div className="divider px-3"></div>
-            <Conversations/>
+            <Conversations search={search} />
             <LogoutButton />
         </div>
     )
